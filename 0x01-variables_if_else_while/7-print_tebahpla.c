21e7a41c76264b6ffe195666dcbaa19dd927f5bd
codeThinker <tiagoafonso110@gmail.com>
2022-06-03 16:03:44 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x > 'a'; x--)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
