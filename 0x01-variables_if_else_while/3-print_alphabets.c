c4659df8589fa4e34adee617850ece7ecccd6f74
codeThinker <tiagoafonso110@gmail.com>
2022-06-03 15:04:47 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
