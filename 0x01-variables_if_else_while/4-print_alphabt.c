749cbd3cd1d3f4f21125b07bfec1216f611d3a09
codeThinker <tiagoafonso110@gmail.com>
2022-06-03 15:36:10 -0700

    alph

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 	int x;
 
 	for (x = 'a'; x <= 'z'; x++)
-	{       
-		if (x != 'e' || x != 'q')
-		{
-			putchar(x);
-		}
+	{
+	if (x != 'e' && x != 'q')
+	{
+	putchar(x);
+	}
 	}
 	putchar('\n');
 	return (0);
