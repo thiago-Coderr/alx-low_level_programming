18de125e9236533a58e608641e1bcd3ed5c67257
codeThinker <tiagoafonso110@gmail.com>
2022-06-03 16:50:46 -0700

    ell

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,13 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
-	if (x == 9)
+	putchar(x + '0');
+	if (x < 9)
 	{
-	putchar(x % 10 + '0');
-	}
-	else
-	{
-         putchar(x % 10 + '0');
-	 putchar(',' + ' ');
+	putchar(',');
+	putchar(' ');
 	}
 	}
 	putchar('\n');
