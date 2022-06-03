f10e7c90b9d8ea91ab4e35c867fa50806815d54d
codeThinker <tiagoafonso110@gmail.com>
2022-06-03 15:16:12 -0700

    alph

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
