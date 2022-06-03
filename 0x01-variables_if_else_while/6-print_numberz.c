094fb9a0a8bec8d8810bd52aae5b83be101e1fa7
codeThinker <tiagoafonso110@gmail.com>
2022-06-03 15:54:17 -0700

    second number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i % 10 + '0');
+	}
+	putchar('\n');
+	return (0);
+}
