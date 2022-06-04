28afde18db56e145b370990ed8fc4a0c2e1637a2
codeThinker <tiagoafonso110@gmail.com>
2022-06-03 17:31:10 -0700

    andre

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print combinations of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hunds, tens, ones;
+
+	for (hunds = 0; hunds <= 9; hunds++)
+	{
+		for (tens = huns + 1; tens <= 9; tens++)
+		{
+			for (ones = tens +1; ones <= 9; ones++)
+			{
+				putchar(hunds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (hunds < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
