1532da661d14ab8bbd92ad49de528a60128873d9
codeThinker <tiagoafonso110@gmail.com>
2022-06-07 14:01:42 -0700

    alp

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@
 void print_alphabet(void)
 {
 	int i;
-	char alph[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+	char alph[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 	'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	for (i = 0; i < 26; i++)
