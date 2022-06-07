28053ee1efb230dcc23d852c172bf643fa3dd713
codeThinker <tiagoafonso110@gmail.com>
2022-06-07 13:33:56 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+	char alph[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alph[i]);
+	}
+	_putchar('\n');
+}
