a35fc5ac530115dc843c700a03869ed4598d3bf0
codeThinker <tiagoafonso110@gmail.com>
2022-06-08 14:52:45 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,45 +1,41 @@
 #include "main.h"
 
+/**
+ * jack_bauer - prints minutes of the day
+ * Return: void
+ */
 void jack_bauer()
 {
-	int h, m;
+	int h1 = 0, h2 = 0;
+	int m1, m2;
+	int a = 9;
 
-	for (h = 0; h <= 23; h++)
+	while (h2 <= 2)
 	{
-		for (m = 0; m <= 59; m++)
+		if (h2 == 2)
 		{
-			if (h < 10)
-			{
-				_putchar('0');
-				_putchar(h + '0');
-				if (m < 10)
-				{
-					_putchar(':');
-					_putchar('0');
-					_putchar(m + '0');
-				}
-				else
-				{
-					_putchar(':');
-					_putchar(m + '0');
-				}
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(h + '0');
-				if (m < 10)
-				{
-					_putchar(':');
-					_putchar('0');
-					_putchar(m + '0');
-				}
-				else
-				{
-					_putchar(':');
-					_putchar(m + '0');
-				}
-			}
+			a = 3;
 		}
+		while (h1 <= a)
+		{
+			m2 = 0;
+			while (m2 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar(h2 + '0');
+					_putchar(h1 + '0');
+					_putchar(':');
+					_putchar(m2 + '0');
+					_putchar(m1 + '0');
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
+			}
+			h1++;
+		}
+		h2++;
 	}
 }
