2c13f97a32c530607b9431efc5fffbcde2088617
codeThinker <tiagoafonso110@gmail.com>
2022-06-07 22:35:36 -0700

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,7 +4,17 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = n % 10;
-	_putchar(last + '0');
-	return (last);
+	if (n < 0)
+	{
+		n = (-1) * n;
+		last = n % 10;
+		_putchar(last + '0');
+		return (last);
+	}
+	else
+	{
+		last = n % 10;
+        	_putchar(last + '0');
+	        return (last);
+	}
 }
