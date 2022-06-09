95e31d526acbc98278330f686632769f75ee1245
codeThinker <tiagoafonso110@gmail.com>
2022-06-09 12:37:40 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * more_numbers - prints ten times nuber list
- * Return: no return;
+ * Return: void
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
