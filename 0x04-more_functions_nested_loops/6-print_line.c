8e89e601a285efa816985a691e0ed2e0b87a429b
codeThinker <tiagoafonso110@gmail.com>
2022-06-09 09:11:48 -0700

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,9 +9,14 @@ void print_line(int n)
 {
 	int i;
         
-	n = n - 1;
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++){
+			_putchar('_');
+		}
 	}
 }
