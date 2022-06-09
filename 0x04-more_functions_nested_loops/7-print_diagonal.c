a4ab0f7e1b337bb66cb86be765029868e6a52c05
codeThinker <tiagoafonso110@gmail.com>
2022-06-09 10:38:48 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the diagonal
+ * @n: is the number of breaked lines
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\\');
+		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i + 1; j++)
+			{   
+				_putchar(' ');
+			}
+		}
+	}
+}
