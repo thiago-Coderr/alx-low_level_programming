0fb48202b97e3ba270894d165ad44bf12b66a775
codeThinker <tiagoafonso110@gmail.com>
2022-06-09 06:18:28 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - receive two number and multiply tem
+ * @a: the first number
+ * @b: the second number
+ * Return: returns the product of both a anb
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
