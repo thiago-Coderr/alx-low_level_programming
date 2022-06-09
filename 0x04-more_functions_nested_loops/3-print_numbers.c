54eb0d5cfff697f69836bd0e0ed00084430c5663
codeThinker <tiagoafonso110@gmail.com>
2022-06-09 06:58:18 -0700

    num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,12 @@
  * print_numbers - prints from 0 to 9
  * Return : No return
  */
+
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
 	}
