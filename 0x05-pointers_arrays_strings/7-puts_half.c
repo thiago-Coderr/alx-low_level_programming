8811f91760f40f7c7bce8baf3909dd017509fb87
thiago-Coderr <92113367+thiago-Coderr@users.noreply.github.com>
2022-06-13 18:56:47 +0100

    Update 7-puts_half.c
    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,40 +1,32 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
-
+#include<stdio.h>
+#include<string.h>
 /**
- * puts_half - prints only the half of a string
- * @str: the parameter
- * Return: void
+ * puts_half - function
+ * @str: will be used
  */
 void puts_half(char *str)
 {
-	int i, tam, half;
-
-	tam = strlen(str);
-	if (str[0] == '\0')
-	{
-		printf("\n");
-	}
-	else
-	{
-		if (tam % 2 == 0)
-		{
-			half = tam / 2;
-			for (i = half; i < tam; i++)
-			{
-				printf("%c", *(str + i));
-			}
-			printf("\n");
-		}
-		else
-		{
-			half = ((tam - 1) / 2) + 1);
-			for (i = half; i < tam; i++)
-			{
-				printf("%c", *(str + i));
-			}
-			printf("\n");
-		}
-	}
+int len, i;
+len = strlen(str);
+if (len % 2 == 0)
+{
+for (i = len / 2; i < len; i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}
+else if (len == 0)
+{
+printf("\n");
+}
+else
+{
+for (i = ((len - 1) / 2) + 1; i < len; i++)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}
 }
