084e98b2d29cc4a84d9e4c698abf8f1111dfe94a
codeThinker <tiagoafonso110@gmail.com>
2022-06-13 09:03:51 -0700

    strinr

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - the function name
+ * @s: is the parameter
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, len, temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tem = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}	
