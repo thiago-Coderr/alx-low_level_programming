7b56d923d1904faf41ba9e1c676f03eb8f99d66a
codeThinker <tiagoafonso110@gmail.com>
2022-06-13 06:58:50 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - prints only the half of a string
+ * @str: the parameter
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i; tam;
+	int half;
+
+	tam = strlen(str);
+	if (str[0] == '\0')
+	{
+		printf("\n");
+	}
+	else
+	{
+		if (tam % 2 == 0)
+		{
+			half = tam / 2;
+		        for (i = half; i < tam; i++)
+			{
+				printf("%c", *(str + i));
+			}
+			printf("\n");
+		}
+		else
+		{
+			half = (tam - 1) / 2;
+			for (i = half; i < tam; i++)
+			{
+				printf("%c", *(str + i));
+			}
+			printf("\n");
+		}
+	}
+}
