f85aa3fc2111c64b208e18f473defcc1e700f579
codeThinker <tiagoafonso110@gmail.com>
2022-06-13 04:49:10 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - gives thr lenth of a string
+ * @s: is the string parameter
+ * Return: Always an integer
+ */
+int _strlen(char *s)
+{
+	int tam;
+
+	tam = strlen(s);
+	return (tam);
+}	
