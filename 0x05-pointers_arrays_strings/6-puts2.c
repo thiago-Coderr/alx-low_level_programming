08aad4e18fd6ba0beb98190c1fb09e3c1db95a98
codeThinker <tiagoafonso110@gmail.com>
2022-06-13 06:28:13 -0700

    only even

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,11 +12,19 @@ void puts2(char *str)
 	int i, tam;
 
 	tam = strlen(str);
-	for (i = 0; i < tam; i++)
+	if (str[0] == '\0')
 	{
-		if (i % 2 == 0)
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i < tam; i++)
 		{
-			printf("%c", *(str + i));
+			if (i % 2 == 0)
+			{
+				printf("%c", *(str + i));
+			}
 		}
+		printf("\n");
 	}
 }
